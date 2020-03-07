e4f81439bd4e0f41db720d20cccd4cec9da4b505
vishal pathode <vishalpathode@hotmail.com>
2020-03-07 13:12:49 +0530

    user 222 sy

--- src/com/demo/Executable.java
+++ src/com/demo/Executable.java
@@ -5,23 +5,7 @@ public class Executable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("use 111 commited");
-		System.out.println("use 111 commited");
-		System.out.println("use 111 commited");
-		if(true){
-		}
-		
-		System.out.println("use 222 commited");
-		System.out.println("use 222 commited");
-		System.out.println("adasjkdhj sjashk");
-		if(true){
-			
-		}else{
-			
-		}
-		System.out.println("------");
-		System.out.println("-+++++++=====----");
-		System.out.println("-+++++++ 3333 =====----");
-		System.out.println("-+++++++ 44444 =====----");
+
 
 	}
 
