edc57384ff6763556f25a573ea51ef93bab3ecca
vishal pathode <vishalpathode@hotmail.com>
2020-03-07 11:25:20 +0530

    user 222

--- src/com/demo/Executable.java
+++ src/com/demo/Executable.java
@@ -4,7 +4,7 @@ public class Executable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("user 2");
+		System.out.println("user 2222");
 	}
 
 }
