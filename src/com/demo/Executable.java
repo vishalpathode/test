1f401a86c35a6ffb6326a55c29353f5d6b5a2576
vishal pathode <vishalpathode@hotmail.com>
2020-03-07 15:35:27 +0530

    user 222 sy

--- src/com/demo/Executable.java
+++ src/com/demo/Executable.java
@@ -5,20 +5,8 @@ public class Executable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		if(true){
-			System.out.println("");
-			System.out.println("");
-		}else{
 			
 		}
-
-		if(true){
-			System.out.println("");
-		}else{
-			
-		}
-		System.out.println("");
-
-		
 	}
 
 }
